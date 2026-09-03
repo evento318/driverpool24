5f5835d80a184b92a363a93e41ec35bbf7c19d33
evento318 <eventofullservice@gmail.com>
2026-09-03 14:38:39 +0200

    Convert server.js to ES module (use import + import.meta.url)

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
